ba15985af64be7c10863092b5b43796d137d7c34
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-12-17 16:39:43 +0100

    added django filter

--- base/settings.py
+++ base/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djoser',
     'django_filters',
     'drf_yasg',
     'customuser',
