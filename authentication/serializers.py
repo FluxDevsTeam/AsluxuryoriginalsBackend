5edab7df4f6c25cfb355078046d87376b1179c47
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-12-18 10:42:03 +0100

    added authentication serializers

---
+++ authentication/serializers.py
@@ -0,0 +1,71 @@
+from rest_framework import serializers
+from .models import User
+
+
+# class UserSignupSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('first_name', 'last_name', 'email', 'password')
+#
+#     extra_kwargs = {
+#         'first_name': {'required': True, 'allow_blank': False},
+#         'last_name': {'required': True, 'allow_blank': False},
+#         'email': {'required': True, 'allow_blank': False},
+#         'password': {'required': True, 'allow_blank': False},
+#     }
+
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    # tokens = serializers.SerializerMethodField()
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name',
+                  'username', 'email', 'password', 'tokens']
+        read_only_fields = ['id', ]
+
+    extra_kwargs = {
+        'first_name': {'required': True, 'allow_blank': False},
+        'last_name': {'required': True, 'allow_blank': False},
+        'email': {'required': True, 'allow_blank': False},
+        'password': {'required': True, 'allow_blank': False},
+    }
+
+
+class EmailVerificationSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(max_length=555)
+
+    class Meta:
+        model = User
+        fields = ['token']
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=50, min_length=6, write_only=True)
+    email = serializers.EmailField(max_length=50, min_length=2)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'email', 'username')
+
+
+from rest_framework import serializers
+
+
+class ForgotPasswordSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=100)
+
+
+class CheckOTPSerializer(serializers.Serializer):
+    otp = serializers.CharField(max_length=6)
+    token = serializers.CharField()
+
+
+class CheckSignupOTPSerializer(serializers.Serializer):
+    otp = serializers.CharField(max_length=6)
+    token = serializers.CharField()
