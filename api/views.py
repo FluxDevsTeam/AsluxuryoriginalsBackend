fdc198d2d638be766c6327a142cc85e04dfaacd4
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-12-18 10:59:27 +0100

    added views.py to api app

--- api/views.py
+++ api/views.py
@@ -1,3 +1,170 @@
-from django.shortcuts import render
+import profile
+import uuid
 
-# Create your views here.
+from django.conf import settings
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from storeapp.models import Product, Category, Review, Cart, Cartitems, Profile, Order
+from .filters import ProductFilter
+from .serializers import ProductSerializer, CategorySerializer, ReviewSerializer, CartSerializer, CartItemSerializer, \
+    AddCartItemSerializer, UpdateCartItemSerializer, ProfileSerializer, OrderSerializer, CreateOrderSerializer, UpdateOrderSerializer
+from rest_framework.parsers import MultiPartParser, FormParser
+import requests
+
+
+def initiate_payment(amount, email, order_id):
+    url = "https://api.flutterwave.com/v3/payments"
+    headers = {
+        "Authorization": f"Bearer {settings.FLW_SEC_KEY}"
+    }
+
+    data = {
+        "tx_ref": str(uuid.uuid4()),
+        "amount": str(amount),
+        "currency": "NGN",
+        "redirect_url": "http:/127.0.0.1:8000/api/orders/confirm_payment/?o_id=" + order_id,
+        "meta": {
+            "consumer_id": 23,
+            "consumer_mac": "92a3-912ba-1192a"
+        },
+        "customer": {
+            "email": email,
+            "phonenumber": "080****4528",
+            "name": "Yemi Desola"
+        },
+        "customizations": {
+            "title": "Shopy Naija",
+            "logo": "https://i5.walmartimages.com/asr/204dad21-0828-4e1e-8b82-b0c546ca3565_2.61e1b358993207fe8d6c97bb9fc2687e.png"
+        }
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response_data = response.json()
+        return Response(response_data)
+
+    except requests.exceptions.RequestException as err:
+        print("the payment didn't go through")
+        return Response({"error": str(err)}, status=500)
+
+
+class ApiProducts(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['category_id', 'price']
+    filterset_class = ProductFilter
+    search_fields = ['name', 'description']
+    ordering_fields = ['price']
+    pagination_class = PageNumberPagination
+
+
+class ApiProductsReviews(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
+
+
+class ApiCart(viewsets.GenericViewSet, CreateModelMixin, RetrieveModelMixin, DestroyModelMixin):
+    serializer_class = CartSerializer
+    queryset = Cart.objects.all()
+
+
+class ApiCartItem(viewsets.ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
+        return CartItemSerializer
+
+    def get_queryset(self):
+        return Cartitems.objects.filter(cart_id=self.kwargs['cart_pk'])
+
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
+
+
+class APICategory(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class ApiProfile(viewsets.ModelViewSet):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+    parser_classes = MultiPartParser, FormParser
+
+    def create(self, request, *args, **kwargs):
+        name = request.data['name']
+        bio = request.data['bio']
+        picture = request.data['picture']
+        Profile.objects.create(name=name, bio=bio, picture=picture)
+        return Response('profile created successfully', status=status.HTTP_200_OK)
+
+
+class ApiOrder(viewsets.ModelViewSet):
+    http_method_names = ["get", "patch", "post", "delete", "options", "head"]
+
+    @action(detail=True, methods=['POST'])
+    def pay(self, request, pk):
+        order = self.get_object()
+        amount = order.total_price
+        email = request.user.email
+        order_id = str(order.id)
+        # redirect_url = "http://127.0.0.1:8000/confirm"
+        return initiate_payment(amount, email, order_id)
+
+    @action(detail=False, methods=["POST"])
+    def confirm_payment(self, request):
+        order_id = request.GET.get("o_id")
+        order = Order.objects.get(id=order_id)
+        order.pending_status = "C"
+        order.save()
+        serializer = OrderSerializer(order)
+
+        data = {
+            "msg": "payment was successful",
+            "data": serializer.data
+        }
+        return Response(data)
+
+    def get_permissions(self):
+        if self.request.method in ["PATCH", "DELETE"]:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(data=request.data, context={"user_id": self.request.user.id})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateOrderSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateOrderSerializer
+        return OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Order.objects.all()
+        return Order.objects.filter(owner=user)
+
