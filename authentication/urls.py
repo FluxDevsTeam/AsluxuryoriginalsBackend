8156520517ebafdfca873eca1dff32988f3d5e8a
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-12-18 10:42:47 +0100

    added authentication urls endpoints

---
+++ authentication/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path, include
+from .views import (UserSignupViewSet, UserLoginViewSet, SendOTPViewSet, CheckOTPViewSet, CheckSignupOTPViewSet,
+                    LogoutViewSet)
+from rest_framework_simplejwt.views import TokenRefreshView
+urlpatterns = [
+    path('signup/', UserSignupViewSet.as_view({'post': 'create'}), name='UserSignupViewSet'),
+    path('login/', UserLoginViewSet.as_view({'post': 'create'}), name='UserLoginViewSet'),
+    path('forgot_password/', SendOTPViewSet.as_view({'post': 'create'}), name='forgot-password'),
+    path('check_otp/', CheckOTPViewSet.as_view({'post': 'create'}), name='check-otp'),
+    path('check_signup_otp/', CheckSignupOTPViewSet.as_view({'post': 'create'}), name='check-signup-otp'),
+    path('api/logout/', LogoutViewSet.as_view({'post': 'logout'}), name='logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('accounts/', include('allauth.urls')),
+]
