84b55d3bf38058e9c5fd01dd533944f5cd28d07d
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-12-19 11:39:14 +0100

    setup user profie in customuser app

--- customuser/models.py
+++ customuser/models.py
@@ -1,44 +1,58 @@
-import email
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-# Create your models here.
-
 class UserManager(BaseUserManager):
+    """
+    Custom manager for the User model where email is the unique identifier.
+    """
 
     def _create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('The given email must be set')
+            raise ValueError("The email field must be set")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        user.set_password(password)
+        if password:
+            user.set_password(password)
+        else:
+            raise ValueError("Password must be provided")
         user.save(using=self._db)
         return user
 
     def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and return a regular user.
+        """
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
+        """
+        Create and return a superuser.
+        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+        if not extra_fields['is_staff']:
+            raise ValueError("Superuser must have is_staff=True.")
+        if not extra_fields['is_superuser']:
+            raise ValueError("Superuser must have is_superuser=True.")
 
         return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
-    username = None
+    """
+    Custom user model where email is the unique identifier for authentication.
+    """
+    username = None  # Remove username field
     email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'  # Use email as the unique identifier
+    REQUIRED_FIELDS = []  # No additional required fields
 
     objects = UserManager()
+
+    def __str__(self):
+        return self.email
