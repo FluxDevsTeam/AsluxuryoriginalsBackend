4580bb1d64c88fa232a404588ec781d8571aaaf4
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-12-22 15:15:43 +0100

    setup database model

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.text import slugify
+from autoslug import AutoSlugField
 import uuid
 from django.conf import settings
 
@@ -7,18 +7,7 @@ from django.conf import settings
 class Category(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True, blank=True, null=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            base_slug = slugify(self.title)
-            slug = base_slug
-            counter = 1
-            while Category.objects.filter(slug=slug).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-            self.slug = slug
-        super().save(*args, **kwargs)
+    slug = AutoSlugField(populate_from='title', unique=True, db_index=True)
 
     def __str__(self):
         return self.title
@@ -29,24 +18,13 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     discount = models.BooleanField(default=False)
-    image = models.ImageField(upload_to='img', blank=True, null=True, default='')
-    price = models.FloatField(default=100.00)
+    image = models.ImageField(upload_to='img', blank=True, null=True, default='placeholder.jpg')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=100.00)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name='products')
-    slug = models.SlugField(unique=True, blank=True, null=True)
+    slug = AutoSlugField(populate_from='name', unique=True, db_index=True)
     inventory = models.IntegerField(default=5)
     top_deal = models.BooleanField(default=False)
     flash_sales = models.BooleanField(default=False)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            base_slug = slugify(self.name)
-            slug = base_slug
-            counter = 1
-            while Product.objects.filter(slug=slug).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-            self.slug = slug
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -55,66 +33,36 @@ class Product(models.Model):
 class ProductImages(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='img', default='', null=True, blank=True)
-    slug = models.SlugField(unique=True, blank=True, null=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            base_slug = slugify(f"{self.product.name}-other-image")
-            slug = base_slug
-            counter = 1
-            while ProductImages.objects.filter(slug=slug).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-            self.slug = slug
-        super().save(*args, **kwargs)
+    image = models.ImageField(upload_to='img', default='placeholder.jpg', null=True, blank=True)
+    slug = AutoSlugField(
+        populate_from=lambda instance: f"{instance.product.name}-other-image", unique=True, db_index=True
+    )
 
     def __str__(self):
         return f"{self.product.name} extra image"
-
 
 
 class Cart(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="carts")
     created = models.DateTimeField(auto_now_add=True)
-    slug = models.SlugField(unique=True, blank=True, null=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            base_slug = slugify(f"{self.user.username}-cart")
-            slug = base_slug
-            counter = self.user.carts.count() + 1
-            while Cart.objects.filter(slug=slug).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-            self.slug = slug
-        super().save(*args, **kwargs)
+    slug = AutoSlugField(populate_from=lambda instance: f"{instance.user.username}-cart", unique=True, db_index=True)
 
     def __str__(self):
         return f"Cart {self.id} ({self.slug})"
 
 
-class Cartitems(models.Model):
+class CartItems(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items", null=True, blank=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True, related_name='cartitems')
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cartitems')
     quantity = models.PositiveSmallIntegerField(default=0)
-    slug = models.SlugField(unique=True, blank=True, null=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            base_slug = slugify(f"{self.cart.slug}-{self.product.name}")
-            slug = base_slug
-            counter = 1
-            while Cartitems.objects.filter(slug=slug).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-            self.slug = slug
-        super().save(*args, **kwargs)
+    slug = AutoSlugField(
+        populate_from=lambda instance: f"{instance.cart.slug}-{instance.product.name}", unique=True, db_index=True
+    )
 
     def __str__(self):
-        return f"Cartitem #{self.product.name} ({self.slug})"
+        return f"CartItem #{self.product.name} ({self.slug})"
 
 
 class Order(models.Model):
@@ -122,25 +70,18 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="orders")
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    slug = models.SlugField(unique=True, blank=True, null=True)
+    slug = AutoSlugField(populate_from=lambda instance: f"{instance.owner.username}-order", unique=True, db_index=True)
+
+    def calculate_total_price(self):
+        self.total_price = sum(item.quantity * item.price for item in self.items.all())
+        return self.total_price
 
     def save(self, *args, **kwargs):
-        items = self.items.all()
-        self.total_price = sum(item.quantity * item.price for item in items)
-
-        if not self.slug:
-            base_slug = slugify(f"{self.owner.username}-order")
-            slug = base_slug
-            counter = self.owner.orders.count() + 1
-            while Order.objects.filter(slug=slug).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-            self.slug = slug
-
+        self.calculate_total_price()
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Order #{self.id} ({self.slug}) - Total: ${self.total_price:.2f}"
+        return f"Order #{self.id} ({self.slug}) - Total: ₦{self.total_price:.2f}"
 
 
 class OrderItem(models.Model):
@@ -149,25 +90,14 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    slug = models.SlugField(unique=True, blank=True, null=True)
+    slug = AutoSlugField(
+        populate_from=lambda instance: f"{instance.order.id}-{instance.product.name}", unique=True, db_index=True
+    )
 
     def save(self, *args, **kwargs):
         if not self.pk:
             self.price = self.product.price
-
-        if not self.slug:
-            base_slug = slugify(f"{self.order.id}-{self.product.name}")
-            slug = base_slug
-            counter = 1
-            while OrderItem.objects.filter(slug=slug).exists():
-                slug = f"{base_slug}-{counter}"
-                counter += 1
-            self.slug = slug
-
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.quantity} x {self.product.name} @ NGN{self.price} (Order)"
-
-
-
+        return f"{self.quantity} x {self.product.name} @ ₦ {self.price} (Order)"
