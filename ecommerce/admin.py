fcfa1b476842b5f55c21c42df1289feb5107723b
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-12-30 21:37:05 +0100

    changed  models admin.py

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -2,19 +2,12 @@ from django.contrib import admin
 from .models import *
 
 
-# Register your models here.
-
-class ProductAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
-
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
+admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(CartItems)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Size)
+admin.site.register(ProductImages)
+admin.site.register(SubCategory)
