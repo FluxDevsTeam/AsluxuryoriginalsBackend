791d40b149c102abf3059778ebf3ce2b2c14a47e
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-12-30 21:37:57 +0100

    changed  api urls.py file

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
-from .views import ApiProducts, ApiCart, ApiCartItem, ApiColour, ApiCategory, ApiSize, ApiOrder
+from .views import ApiProducts, ApiCart, ApiCartItem, ApiCategory, ApiSize, ApiOrder, ApiSubCategory
 
 # Default router for general API views
 router = DefaultRouter()
@@ -9,10 +9,10 @@ router = DefaultRouter()
 # Register main resources like Products, Cart, Categories, etc.
 router.register('products', ApiProducts, basename='product')
 router.register('categories', ApiCategory, basename='category')
-router.register('colours', ApiColour, basename='colour')
 router.register('sizes', ApiSize, basename='size')
 router.register('orders', ApiOrder, basename='order')
-router.register('carts', ApiCart, basename='cart')  # Register 'carts' first
+router.register('carts', ApiCart, basename='cart')
+router.register('subcategory', ApiSubCategory, basename='subcategory')
 
 # Nested router for Cart and CartItems (now 'carts' is registered above)
 cart_router = NestedDefaultRouter(router, 'carts', lookup='cart')
