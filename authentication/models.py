d8e3d7bdbeb8f9f9460be4c99eefd8110caa7d74
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-02 21:09:37 +0100

    created models for authentication

---
+++ authentication/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from customuser.models import User
+
+
+class EmailChangeRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='email_change_requests')
+    new_email = models.EmailField()
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Email change request for {self.user.username} to {self.new_email}"
+
+
+class PasswordChangeRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='password_change_requests')
+    otp = models.CharField(max_length=6)
+    new_password = models.CharField(max_length=128)  # To securely store hashed password
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Password change request for {self.user.username}"
