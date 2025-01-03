86e5b435b601ab207e34b51457a6839cde24f5f1
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-03 14:43:40 +0100

    changed authentication models to handle user profile first and last name change

--- authentication/models.py
+++ authentication/models.py
@@ -2,6 +2,27 @@ from django.db import models
 from customuser.models import User
 
 
+class NameChangeRequest(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="name_change_request")
+    new_first_name = models.CharField(max_length=150, null=True, blank=True)
+    new_last_name = models.CharField(max_length=150, null=True, blank=True)
+    otp = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Name Change Request for {self.user.email} - {self.new_first_name} {self.new_last_name}"
+
+
+class EmailChangeRequest(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="email_change_request")
+    new_email = models.EmailField(unique=True)
+    otp = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Email Change Request for {self.user.email} to {self.new_email}"
+
+
 class ForgotPasswordRequest(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     otp = models.IntegerField()
@@ -10,17 +31,6 @@ class ForgotPasswordRequest(models.Model):
 
     def __str__(self):
         return f"ForgotPasswordRequest for {self.user.email}"
-
-
-class EmailChangeRequest(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='email_change_requests')
-    new_email = models.EmailField()
-    otp = models.CharField(max_length=6)
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_verified = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"Email change request for {self.user.username} to {self.new_email}"
 
 
 class PasswordChangeRequest(models.Model):
