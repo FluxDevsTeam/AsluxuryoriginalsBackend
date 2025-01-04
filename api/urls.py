0b73c70db5a51721ab31c1fdbd193a38bc8e5f62
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-04 09:01:17 +0100

    worked on api urls. added dashboard endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
-from .views import ApiProducts, ApiCart, ApiCartItem, ApiCategory, ApiOrder, ApiSubCategory
+from .views import ApiProducts, ApiCart, ApiCartItem, ApiCategory, ApiOrder, ApiSubCategory, DashboardOrderViewSet
 
 # Default router for general API views
 router = DefaultRouter()
@@ -20,4 +20,9 @@ cart_router.register('items', ApiCartItem, basename='cart-items')
 urlpatterns = [
     path('', include(router.urls)),  # Main API urls
     path('', include(cart_router.urls)),  # Nested urls for Cart and CartItems
+    path('dashboard/', DashboardOrderViewSet.as_view({'get': 'list'}), name='dashboard'),
+    path('dashboard/summary/', DashboardOrderViewSet.as_view({'get': 'summary'}),
+         name='summary'),
+    path('dashboard/most-sold-products/', DashboardOrderViewSet.as_view({'get': 'most_sold_products'}),
+         name='most_sold_products'),
 ]
