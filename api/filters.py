a714ab5a951dc9fee8107c2535ab226ac348d1d3
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-04 08:56:08 +0100

    changed api filters to filter the order

--- api/filters.py
+++ api/filters.py
@@ -1,11 +1,29 @@
-from django_filters.rest_framework import FilterSet
-from ecommerce.models import Product
+from django_filters import DateFilter
+from django_filters.rest_framework import FilterSet, CharFilter, NumberFilter
+from ecommerce.models import Product, Order
 
 
 class ProductFilter(FilterSet):
+    category_id = NumberFilter(field_name='category__id', lookup_expr='exact')
+    category_name = CharFilter(field_name='category__title', lookup_expr='exact')
+    subcategory_id = NumberFilter(field_name='subcategory__id', lookup_expr='exact')
+    subcategory_name = CharFilter(field_name='subcategory__title', lookup_expr='exact')
+
     class Meta:
         model = Product
         fields = {
-            'category_id': ['exact'],
-            'price': ['gt', 'lt']
+            'price': ['gt', 'lt'],  # Only include fields that are directly on the Product model
         }
+
+
+class OrderFilter(FilterSet):
+    month = NumberFilter(field_name='placed_at', lookup_expr='month')
+    year = NumberFilter(field_name='placed_at', lookup_expr='year')
+    start_date = DateFilter(field_name='placed_at', lookup_expr='gte')
+    end_date = DateFilter(field_name='placed_at', lookup_expr='lte')
+
+    class Meta:
+        model = Order
+        fields = ['month', 'year', 'start_date', 'end_date']
+
+
