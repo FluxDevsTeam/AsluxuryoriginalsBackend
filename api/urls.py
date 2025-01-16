6d7e734d6d03d11aa87026c09679ad395c1992d3
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-16 17:22:50 +0100

    removed redirecturl from urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
-from .views import ApiProducts, ApiCart, ApiCartItem, ApiCategory, ApiOrder, ApiSubCategory, DashboardOrderViewSet, payment_redirect_handler
+from .views import ApiProducts, ApiCart, ApiCartItem, ApiCategory, ApiOrder, ApiSubCategory, DashboardOrderViewSet
 
-# Default router for general API views
 router = DefaultRouter()
 
-# Register main resources like Products, Cart, Categories, etc.
 router.register('products', ApiProducts, basename='product')
 router.register('categories', ApiCategory, basename='category')
 router.register('orders', ApiOrder, basename='order')
@@ -20,7 +18,6 @@ cart_router.register('items', ApiCartItem, basename='cart-items')
 urlpatterns = [
     path('', include(router.urls)),  # Main API urls
     path('', include(cart_router.urls)),  # Nested urls for Cart and CartItems
-    path('payment-redirect/', payment_redirect_handler, name='payment_redirect'),
     path('dashboard/', DashboardOrderViewSet.as_view({'get': 'list'}), name='dashboard'),
     path('dashboard/summary/', DashboardOrderViewSet.as_view({'get': 'summary'}),
          name='summary'),
