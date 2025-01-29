e6e92a60cf9758dd4001ecd6048cd54daa43a4ab
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-29 15:47:08 +0100

    install_as_MySQLdb()

--- base/wsgi.py
+++ base/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import pymysql
+pymysql.install_as_MySQLdb()
 
 from django.core.wsgi import get_wsgi_application
 
